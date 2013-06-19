837809ae239dba9672bcf534def9be661591bf7d
Rick Umali <rickumali@gmail.com>
2013-06-18 22:38:55 -0400

    Began retrieving x and y of letter.
    This is just console.log() code, but this shows how I can
    retrieve the x and y of a selected letter.
    
    Also, just as important, I added the 'first' and 'second'
    class attributes to the first and second selected letter.
    This enables me to reference the letter easily.

--- gameblock.js
+++ gameblock.js
@@ -4,8 +4,16 @@ $(document).ready(function() {
   var selectedSecondChar = false;
 
   $("td").hover(function() {
+    if (selectedSecondChar) {
+      return;
+    }
     $(this).addClass("green");
-    console.log(jQuery.data($(this)[0], "data"));
+    if (selectedFirstChar) {
+      console.log("1   X: " + jQuery.data($(".first")[0], "data").x);
+      console.log("1   Y: " + jQuery.data($(".first")[0], "data").y);
+      console.log("CUR X: " + jQuery.data($(this)[0], "data").x);
+      console.log("CUR Y: " + jQuery.data($(this)[0], "data").y);
+    }
   },function() {
     $(this).removeClass("green");
   });
@@ -14,10 +22,12 @@ $(document).ready(function() {
     if (!selectedFirstChar) {
       selectedFirstChar = true;
       $(this).unbind("hover");
+      $(this).addClass("first");
     } else {
       if (!selectedSecondChar) {
         selectedSecondChar = true;
         $(this).unbind("hover");
+        $(this).addClass("second");
 	console.log("Evaluate the word, Rick!");
       }
     }
